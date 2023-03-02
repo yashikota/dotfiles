4ff6d77e0fab58d2b12028a1d739f5f15bb8d03d
kota <52403688+yashikota@users.noreply.github.com>
2023-03-03 06:50:03 +0900

    update config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,11 +2,29 @@ require "plugins"
 
 local opt = vim.opt
 
+-- helpの日本語化
+opt.helplang = "ja,en"
+
+-- 検索時の大文字小文字
+opt.ignorecase = true
+opt.smartcase = true
+
+-- インデント
+opt.smartindent = true
+
+-- ファイル
+opt.autoread = true
+
 -- エンコーディングの指定
 vim.criptencoding = "utf-8"
 
--- 行番号の表示
+-- 行番号
 opt.number = true
+-- opt.relativenumber = true
+opt.signcolumn = "yes"
+
+-- カーソル
+opt.cursorline = true
 
 -- タブの設定
 opt.expandtab = true
