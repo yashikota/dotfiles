e99ea98b0932e09d7f1d54c1e124042eaa0119b6
kota <52403688+yashikota@users.noreply.github.com>
2023-03-07 14:12:48 +0900

    add nvim-treesitter

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -4,5 +4,22 @@ require("packer").startup(function()
     use { "wbthomason/packer.nvim", opt = true }
     use "EdenEast/nightfox.nvim"
     use "lambdalisue/fern.vim"
+    use {
+        "nvim-treesitter/nvim-treesitter",
+        run = function()
+            local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
+            ts_update()
+        end,
+    }
 end)
+
+require("nvim-treesitter.configs").setup {
+  ensure_installed = "all",
+  highlight = {
+    enable = true,
+  },
+  indent = {
+      enable = true
+  },
+}
 
