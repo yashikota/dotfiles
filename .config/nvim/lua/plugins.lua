671c66c5c48d9217d3464982149b74001c2deebb
kota <52403688+yashikota@users.noreply.github.com>
2023-03-09 15:25:30 +0900

    update

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -16,7 +16,80 @@ require("packer").startup(function()
         requires = { "kyazdani42/nvim-web-devicons", opt = true }
     }
     use "lewis6991/gitsigns.nvim"
+    use "lukas-reineke/indent-blankline.nvim"
+    use {
+	    "windwp/nvim-autopairs",
+        config = function() require("nvim-autopairs").setup {} end
+    }
+    use "neovim/nvim-lspconfig"
+    use "williamboman/mason.nvim"
+    use "williamboman/mason-lspconfig.nvim"
+
+    use "hrsh7th/nvim-cmp"
+    use "hrsh7th/cmp-nvim-lsp"
 end)
+
+-- LSP
+require("mason").setup()
+require("mason-lspconfig").setup_handlers({ function(server)
+  local opt = {
+    capabilities = require("cmp_nvim_lsp").update_capabilities(
+      vim.lsp.protocol.make_client_capabilities()
+    )
+  }
+  require("lspconfig")[server].setup(opt)
+end })
+
+vim.keymap.set("n", "K",  "<cmd>lua vim.lsp.buf.hover()<CR>")
+vim.keymap.set("n", "gf", "<cmd>lua vim.lsp.buf.formatting()<CR>")
+vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>")
+vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
+vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>")
+vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>")
+vim.keymap.set("n", "gt", "<cmd>lua vim.lsp.buf.type_definition()<CR>")
+vim.keymap.set("n", "gn", "<cmd>lua vim.lsp.buf.rename()<CR>")
+vim.keymap.set("n", "ga", "<cmd>lua vim.lsp.buf.code_action()<CR>")
+vim.keymap.set("n", "ge", "<cmd>lua vim.diagnostic.open_float()<CR>")
+vim.keymap.set("n", "g]", "<cmd>lua vim.diagnostic.goto_next()<CR>")
+vim.keymap.set("n", "g[", "<cmd>lua vim.diagnostic.goto_prev()<CR>")
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = false }
+)
+
+vim.cmd [[
+set updatetime=500
+highlight LspReferenceText  cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
+highlight LspReferenceRead  cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
+highlight LspReferenceWrite cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
+augroup lsp_document_highlight
+  autocmd!
+  autocmd CursorHold,CursorHoldI * lua vim.lsp.buf.document_highlight()
+  autocmd CursorMoved,CursorMovedI * lua vim.lsp.buf.clear_references()
+augroup END
+]]
+
+local cmp = require("cmp")
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body)
+    end,
+  },
+  sources = {
+    { name = "nvim_lsp" }
+  },
+  mapping = cmp.mapping.preset.insert({
+    ["<C-p>"] = cmp.mapping.select_prev_item(),
+    ["<C-n>"] = cmp.mapping.select_next_item(),
+    ['<C-l>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ["<CR>"] = cmp.mapping.confirm { select = true },
+  }),
+  experimental = {
+    ghost_text = true,
+  },
+})
 
 -- nvim-treesitter
 require("nvim-treesitter.configs").setup {
@@ -36,10 +109,21 @@ vim.g.mapleader = " "
 vim.api.nvim_set_keymap("n", "<Leader>e", ":Fern . -reveal=%<CR>", {noremap=true, silent=true})
 
 -- nightfox
-vim.cmd([[colorscheme duskfox]])
+vim.cmd([[colorscheme carbonfox]])
 
 -- lualine
 require("lualine").setup()
 
 --gitsigns
 require("gitsigns").setup()
+
+-- indent-blankline
+vim.opt.list = true
+vim.opt.listchars:append "space:⋅"
+vim.opt.listchars:append "eol:↴"
+
+require("indent_blankline").setup {
+    show_end_of_line = true,
+    space_char_blankline = " ",
+}
+
