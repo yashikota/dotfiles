982292d731d2ce002a06d9584e32579a34254cc5
kota <52403688+yashikota@users.noreply.github.com>
2023-08-15 08:38:25 +0900

    update

--- setup.sh
+++ setup.sh
@@ -8,20 +8,20 @@ function install_dependencies() {
     sudo apt-get update
     cat apps.txt | while read line
     do
-        type -p $line >/dev/null && echo -e "\e[36m$line is already installed\e[m" && continue
+        type -p $line >/dev/null && echo "\e[36m$line is already installed\e[m" && continue
         sudo apt-get install --no-install-recommends -y $line
     done
 }
 
 # install rust
 function install_rust() {
-    type -p rustup >/dev/null && echo -e "\e[36mrustup is already installed\e[m" && return
+    type -p rustup >/dev/null && echo "\e[36mrustup is already installed\e[m" && return
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 }
 
 # install go
 function install_go() {
-    type -p go >/dev/null && echo -e "\e[36mgo is already installed\e[m" && return
+    type -p go >/dev/null && echo "\e[36mgo is already installed\e[m" && return
     sudo add-apt-repository -y ppa:longsleep/golang-backports
     sudo apt-get update
     sudo apt-get install --no-install-recommends -y golang
@@ -29,7 +29,7 @@ function install_go() {
 
 # install github cli
 function install_github_cli() {
-    type -p gh >/dev/null && echo -e "\e[36mgh is already installed\e[m" && return
+    type -p gh >/dev/null && echo "\e[36mgh is already installed\e[m" && return
     curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
     && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
     && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
@@ -39,34 +39,20 @@ function install_github_cli() {
 
 # install starship
 function install_starship() {
-    type -p starship >/dev/null && echo -e "\e[36mstarship is already installed\e[m" && return
+    type -p starship >/dev/null && echo "\e[36mstarship is already installed\e[m" && return
     curl -fsSL https://starship.rs/install.sh | sh
 }
 
 # install rtx
 function install_rtx() {
-    type -p rtx >/dev/null && echo -e "\e[36mrtx is already installed\e[m" && return
+    type -p rtx >/dev/null && echo "\e[36mrtx is already installed\e[m" && return
     cargo install rtx-cli
 }
 
-# clone dotfiles
-function clone_dotfiles() {
-    if [ -d dotfiles ]; then
-        rm -rf dotfiles
-    fi
-    git clone https://github.com/yashikota/dotfiles.git
-}
-
-# link dotfiles
-function link_dotfiles() {
-    cd dotfiles
-    if [ ! -d ~/.config ]; then
-        mkdir ~/.config
-    fi
-
-    for f in .config/*; do
-        ln -svfn $(pwd)/$f ~/.config/
-    done
+# install poetry
+function install_poetry() {
+    type -p poetry >/dev/null && echo "\e[36mpoetry is already installed\e[m" && return
+    curl -sSL https://install.python-poetry.org | python3 -
 }
 
 # change default shell
@@ -81,9 +67,8 @@ install_go
 install_github_cli
 install_starship
 install_rtx
-clone_dotfiles
-link_dotfiles
+install_poetry
 change_default_shell
 
 # Done
-echo -e "\e[34mDone\e[m"
+echo "\e[34mDone\e[m"
