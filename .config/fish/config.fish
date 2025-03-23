bef5627f1f8737ac8ef59b3dba4fceb053dec38d
yashikota <52403688+yashikota@users.noreply.github.com>
2025-03-23 21:42:04 +0900

    add: aqua global

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -65,6 +65,7 @@ if status is-interactive
     set -gx AQUA_ROOT_DIR "$XDG_DATA_HOME/aquaproj-aqua"
     test -n "$XDG_DATA_HOME"; or set -gx AQUA_ROOT_DIR "$HOME/.local/share/aquaproj-aqua"
     set -gx PATH "$AQUA_ROOT_DIR/bin" $PATH
+    set -gx AQUA_GLOBAL_CONFIG "$HOME/.config/aquaproj-aqua/aqua.yaml"
 
     # wasmtime
     set -gx WASMTIME_HOME "$HOME/.wasmtime"
