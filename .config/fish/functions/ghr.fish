2768d2819251cd3ea7ac858270acadaec206a8d9
yashikota <52403688+yashikota@users.noreply.github.com>
2025-03-30 23:54:07 +0900

    update

--- .config/fish/functions/ghr.fish
+++ .config/fish/functions/ghr.fish
@@ -1,16 +1,19 @@
 function ghr
     set -l organization $argv[1]
     set -l visibility $argv[2]
-    
+
+    if not git rev-parse --is-inside-work-tree 2>/dev/null
+        git init
+    end
+
     if [ "$visibility" = "public" ]
         set visibility "--public"
     else
         set visibility "--private"
     end
-    
+
     set -l current_directory (basename $PWD)
     set -l source_directory (git rev-parse --show-toplevel)
-    
-    gh repo create $organization/$current_directory --source $source_directory $visibility
-end
 
+    gh repo create $organization/$current_directory  $visibility --source $source_directory --remote=upstream
+end
