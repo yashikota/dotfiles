2f3485c2bbf603e39071bf589bcb8dfe85290fba
yashikota <52403688+yashikota@users.noreply.github.com>
2026-01-07 23:05:11 +0900

    fix open command

--- .config/fish/functions/open.fish
+++ .config/fish/functions/open.fish
@@ -1,9 +1,14 @@
 function open
-    if [ -n "$IS_WSL" ] || [ -n "$WSL_DISTRO_NAME" ]
-        explorer.exe $argv[1]
-    else if [ "$(uname)" = "Linux" ]
-        xdg-open $argv[1]
-    else if [ "$(uname)" = "Darwin" ]
-        open $argv[1]
+    set target $argv[1]
+    if test -z "$target"
+        set target .
+    end
+
+    if test -n "$IS_WSL" -o -n "$WSL_DISTRO_NAME"
+        explorer.exe $target
+    else if test (uname) = Linux
+        xdg-open $target
+    else if test (uname) = Darwin
+        command open $target
     end
 end
