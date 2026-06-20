e72ac8a51bf477566669bbbef660544b22b75e74
yashikota <52403688+yashikota@users.noreply.github.com>
2026-06-20 13:56:57 +0000

    update

--- .config/zsh/functions/cmd.zsh
+++ .config/zsh/functions/cmd.zsh
@@ -3,13 +3,21 @@
 # Select an abbreviation to execute its expansion
 cmd() {
     local selected
-    selected=$(abbr list-commands | \
+    local selected_abbr
+    local command
+
+    selected=$(abbr list | \
+        awk -F= '!seen[$1]++ { order[++n] = $1 } { line[$1] = $0 } END { for (i = 1; i <= n; i++) print line[order[i]] }' | \
         fzf --no-sort --query="${*}" \
             --preview-window=hidden \
             --prompt="Abbr > ")
 
     if [[ -n "$selected" ]]; then
-        print -s -- "$selected"
-        eval "$selected"
+        selected_abbr="${selected%%=*}"
+        selected_abbr="${(Q)selected_abbr}"
+        command=$(abbr expand "$selected_abbr") || return
+
+        print -s -- "$command"
+        eval "$command"
     fi
 }
